59563905e4311645dc594e8c52252f5371a2681c
Robert Hardy <robbert@gmail.com>
2022-01-31 11:47:30 +0000

    Display as coloured boxes.

--- src/Main.elm
+++ src/Main.elm
@@ -16,6 +16,61 @@ import Json.Encode as Encode
 
 
 
+fireColorsPalette =
+  Array.fromList
+    [ {r=7, g=7, b=7}
+    , {r=31, g=7, b=7}
+    , {r=47, g=15, b=7}
+    , {r=71, g=15, b=7}
+    , {r=87, g=23, b=7}
+    , {r=103, g=31, b=7}
+    , {r=119, g=31, b=7}
+    , {r=143, g=39, b=7}
+    , {r=159, g=47, b=7}
+    , {r=175, g=63, b=7}
+    , {r=191, g=71, b=7}
+    , {r=199, g=71, b=7}
+    , {r=223, g=79, b=7}
+    , {r=223, g=87, b=7}
+    , {r=223, g=87, b=7}
+    , {r=215, g=95, b=7}
+    , {r=215, g=95, b=7}
+    , {r=215, g=103, b=15}
+    , {r=207, g=111, b=15}
+    , {r=207, g=119, b=15}
+    , {r=207, g=127, b=15}
+    , {r=207, g=135, b=23}
+    , {r=199, g=135, b=23}
+    , {r=199, g=143, b=23}
+    , {r=199, g=151, b=31}
+    , {r=191, g=159, b=31}
+    , {r=191, g=159, b=31}
+    , {r=191, g=167, b=39}
+    , {r=191, g=167, b=39}
+    , {r=191, g=175, b=47}
+    , {r=183, g=175, b=47}
+    , {r=183, g=183, b=47}
+    , {r=183, g=183, b=55}
+    , {r=207, g=207, b=111}
+    , {r=223, g=223, b=159}
+    , {r=239, g=239, b=199}
+    , {r=255, g=255, b=255}
+    ]
+
+
+getColor n =
+  let
+    maybeRGB =
+      Array.get n fireColorsPalette
+
+  in
+  case maybeRGB of
+    Nothing ->
+      {r=0, b=0, g=0}
+
+    Just rec ->
+      rec
+
 -- MAIN
 
 
@@ -89,7 +144,7 @@ tableRows model =
 
 toHtmlRow: Int -> Array Int -> Html Msg
 toHtmlRow idx_row arr =
-  tr [] (Array.toList (Array.indexedMap (\j -> indexedCell idx_row j ) arr) )
+  tr [] (Array.toList (Array.indexedMap (\j -> colouredCell idx_row j ) arr) )
 
 
 indexedCell: Int -> Int -> Int -> Html Msg
@@ -99,6 +154,18 @@ indexedCell idx_row idx_col value =
       (String.fromInt idx_row) ++ "," ++ (String.fromInt idx_col)
   in
   td [] [ text (String.fromInt value), div [ class "pixel-index" ] [ text idxString ] ]
+
+
+colouredCell: Int -> Int -> Int -> Html Msg
+colouredCell idx_row idx_col value =
+  let
+    rgbData =
+      getColor value
+
+    rgbString =
+      "rgb(" ++ (String.fromInt rgbData.r) ++ "," ++ (String.fromInt rgbData.g) ++ "," ++ (String.fromInt rgbData.b) ++ ")"
+  in
+  td [ style "background-color" rgbString ] []
 
 
 view : Model -> Html Msg
